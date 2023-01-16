b3f31ee0e83d48896ecd1eda6222edee058147ed
Camila Heredia <cs.herediavalverde@gmail.com>
2023-01-15 21:56:19 -0300

    :sparkles: Feat: Add images

--- js/moveCards.js
+++ js/moveCards.js
@@ -1,11 +1,18 @@
 const animatedCards = document.querySelectorAll(".interview-card");
+const animatedMembers = document.querySelectorAll(".section-team_member");
+const animatedAdvices = document.querySelectorAll(".tips_card");
 
-animatedCards.forEach((animatedCard) => {
-    let scrollTop = document.documentElement.scrollTop;
-    console.log(scrollTop)
-    if (scrollTop == 0) {
-        animatedCard.style.opacity = 1;
-        animatedCard.classList.add("animated");
-    }
-})
+const moveCards = (cards) => {
+    cards.forEach((card) => {
+        let scrollTop = document.documentElement.scrollTop;
+        if ( scrollTop == 0) {
+            card.style.opacity = 1;
+            card.classList.add("animated");
+        }
+    })
+}
+
+moveCards(animatedCards);
+moveCards(animatedMembers);
+moveCards(animatedAdvices);
 
